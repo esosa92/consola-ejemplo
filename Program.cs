b94d1a5de8ad3d6965e745fd6439399ca3486a4d
emiliano <emilianoAsosa92@gmail.com>
2024-07-15 23:16:01 -0300

    Commit

--- Program.cs
+++ Program.cs
@@ -1,52 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Programa que al iniciar nos pide que ingresemos 2 numeros (ingresa numero 1, ingresa numero 2)
+        //El programa luego pasara a ejecutar cada uno de los metodos de la clase calculadora
+        //Y mostrar el resultado en pantalla 
+        //Punto bonus: Al inciar el progama en vez de pedirnos que ingresemos 2 numeros
+        //Nos pedira que ingresemos un numero de operacion(Ingresa numero de operacion) 1 suma 2 es resta 3 es multiplicar 4 es dividir
+        //En base al numero de operacion procederemos a realizar solo esa operacion.
+        //Luego de mostrado los resultados el programa debe terminar.
+    }
+}
 
-Console.WriteLine("Ingresa tu nombre a continuacion:"); //print de python
-string? nombre = Console.ReadLine(); //Recibo input del usuario
-Console.WriteLine($"Hola {nombre}"); //Esto es una template string
-Console.WriteLine("Ingresa un numero:");
-int numero1 = Int32.Parse(Console.ReadLine());
-Console.WriteLine("Ingresa otro numero:");
-//Unsigned int
+class Calculator
+{
+    public Calculator()
+    {
+    }
 
-//Divison
-var divison = 5 / 5;
-var multiplicaion = 5 * 1;
-//Potencia
-var potencia = 5 * 5; //esto es 5 elevado a la 5ta
-var potenciaCorrecta = Math.Pow(5, 5); //Esto es eqvuicalente a 5^5
-var potenciaCorrecta2 = Math.Pow(5, 2);//esto es equivalente a 5^2
+    private double _lastResult = 0;
+    private static double _lastResultStatic = 0;
 
+    public static string Sarasa()
+    {
+        return "Sarasa";
+    }
 
+    //[Modificadores de access por defecto es internal]
+    public double Add(double a, double b)
+    {
+        _lastResult = a + b;
+        _lastResultStatic = a + b;
+        return _lastResult;
+    }
 
-//1 = 8 bit -> el bit es la unidad minima de computo. 
-//1 bit puede tener los valores 0 y 1.
-//
-//2^cantidad de bits
-//00000000-> puedo representar hasta 256
-//8 bit es igual a un byte
-//2 bit 
-//00 -> 0
-//01 -> 1
-//10 -> 2
-//11 -> 3
-//3 bit puedo representar 
-//000 1
-//001 2
-//010 3
-//011 4
-//100 5
-//101 6
-//110 7
-//111 8
+    public double Sub(double a, double b)
+    {
+        _lastResult = a - b;
+        _lastResultStatic = a + b;
+        return _lastResult;
+    }
 
-//10 -0
-//11 -1
-//00 +0
-//01 +1
+    public double Multiply(double a, double b)
+    {
+        _lastResult = a * b;
+        _lastResultStatic = a + b;
+        return _lastResult;
+    }
+    
+    public double Divide(double a, double b)
+    {
+        _lastResult = a / b;
+        _lastResultStatic = a + b;
+        return _lastResult;
+    }
 
-int b = 255;
-Console.WriteLine((byte)(b+1));
-Console.WriteLine(Int32.MaxValue);
-Console.WriteLine(Int16.MinValue);
+    public double GetLastResult()
+    {
+        return _lastResult;
+    }
 
-Console.WriteLine($"La suma es: {numero1 + numero2}");
+    public double GetLastResultStatic()
+    {
+        return _lastResultStatic;
+    }
+}
+
+class Persona
+{
+    private string _name;
+    private string _surname;
+    private int _age;
+
+    public Persona(string name, string surname, int age)
+    {
+        _name = name;
+        _surname = surname;
+        _age = age;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hola me llamo {_name} {_surname} y tengo {_age}");
+    }
+}
